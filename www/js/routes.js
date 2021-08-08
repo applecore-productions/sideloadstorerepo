6652dd054a5a13ee15620054414ec1b6b72773a3
applecore-productions <63881820+applecore-productions@users.noreply.github.com>
2021-08-07 21:48:04 -0500

    Add files via upload

---
+++ www/js/routes.js
@@ -0,0 +1,73 @@
+
+var routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+  {
+    path: '/form/',
+    url: './pages/form.html',
+  },
+
+
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    componentUrl: './pages/dynamic-route.html',
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = to.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            componentUrl: './pages/request-and-load.html',
+          },
+          {
+            props: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
